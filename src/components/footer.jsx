b6cc74041b8ebef965a5fa419007263f1821e755
Kuldeep-Chotrani <kuldeepramani17@gmail.com>
2022-04-12 16:06:08 +0530

    first-commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+export const Footer = (props) => {
+  
+  return (
+    <div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; 2022 CamHire{' '}
+            <a href='#' rel='nofollow'>
+              Vendout India Private Limited
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/navigation.jsx
@@ -0,0 +1,58 @@
+import mainLogo from '../images/camhire_logo-04.png';
+export const Navigation = (props) => {
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='container'>
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            {' '}
+            <span className='sr-only'>Toggle navigation</span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+          </button>
+          <img src={mainLogo} className='img-responsive' style={{height:"80px", width:"190px"}} alt='' />
+
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            <li>
+              <a href='#features' className='page-scroll'>
+                Features
+              </a>
+            </li>
+            <li>
+              <a href='#about' className='page-scroll'>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#portfolio' className='page-scroll'>
+                Gallery
+              </a>
+            </li>
+            <li>
+              <a href='#testimonials' className='page-scroll'>
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a href='#services' className='page-scroll'>
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
